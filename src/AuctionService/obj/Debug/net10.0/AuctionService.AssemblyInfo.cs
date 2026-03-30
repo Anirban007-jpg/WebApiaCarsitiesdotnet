ac35be3f596a59629dcea0c93929fbb3066a3b00
Anirban007-jpg <abanerjee763@gmail.com>
2026-03-31 00:38:45 +0530

    Closes #4

--- src/AuctionService/obj/Debug/net10.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net10.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e8ac258ebe5a279f9bb976dbc7d3a819fd4d919")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b084d2bdf577782ebd1307bd6aebc6bd88d453a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
